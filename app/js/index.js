885beb147d6d52bcf94850810d0fd767fc5b38d8
Adam Argyle <argyle@google.com>
2020-07-28 15:25:07 -0700

    Initial commit

---
+++ app/js/index.js
@@ -0,0 +1,15 @@
+// (bundle from remote urls) 
+// import Whatever from 'https://unpkg.com/whatever'
+
+// (bundle from npm)
+// import Whatever from 'whatever'
+// import $ from 'blingblingjs'
+
+// (bundle from local)
+// import whatever from './whatever.js'
+
+// postCSS processed css-4-js
+// (file as string)
+// import { default as index_css }  from './css/index.css'
+
+console?.log('💀')
