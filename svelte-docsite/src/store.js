d098b3e08dc87674f035843941ba44cd1640d355
Adam Argyle <argyle@google.com>
2020-08-31 16:32:00 -0700

    fixes state typo

--- svelte-docsite/src/store.js
+++ svelte-docsite/src/store.js
@@ -2,7 +2,7 @@ import { writable } from 'svelte/store'
 
 export const transition = location.hash !== ''
     ? writable(location.hash.slice(1))
-    : writable('out:cicle:hesitate')
+    : writable('out:circle:hesitate')
 
 export const duration = writable(2.5)
 export const easing = writable('cubic-bezier(.25, 1, .30, 1)')
