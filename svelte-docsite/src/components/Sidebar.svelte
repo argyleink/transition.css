c9e278e991e0f40a0e8e9e67ae25fa83d350840c
Adam Argyle <argyle@google.com>
2020-09-01 11:22:32 -0700

    fixes copy icon not pink anymore

--- svelte-docsite/src/components/Sidebar.svelte
+++ svelte-docsite/src/components/Sidebar.svelte
@@ -162,7 +162,7 @@
 
   dl > dd:hover::after,
   dl > dd:focus-within::after {
-    content: url("data:image/svg+xml;charset=UTF-8,<svg xmlns='http://www.w3.org/2000/svg' fill='var(--pink)' viewBox='0 0 24 24'><path d='M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z'/></svg>");
+    content: url("data:image/svg+xml;charset=UTF-8,<svg xmlns='http://www.w3.org/2000/svg' fill='deeppink' viewBox='0 0 24 24'><path d='M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z'/></svg>");
     position: absolute;
     left: -1.75ch;
     inset: 0 0 0 -1.75ch;
