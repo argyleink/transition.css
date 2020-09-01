92872cbd37d34f2f454b5eab160879496e6a1e42
Adam Argyle <argyle@google.com>
2020-09-01 15:58:36 -0700

    restore default to custom in swoop

--- svelte-docsite/src/store.js
+++ svelte-docsite/src/store.js
@@ -2,7 +2,7 @@ import { writable } from 'svelte/store'
 
 export const transition = location.hash !== ''
     ? writable(location.hash.slice(1))
-    : writable('out:circle:hesitate')
+    : writable('in:custom:circle-swoop')
 
 export const duration = writable(2.5)
 export const easing = writable('cubic-bezier(.25, 1, .30, 1)')
